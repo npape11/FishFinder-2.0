445a30d41a80d400fbb6cbc3556e8d1f8a86b665
npape11 <nmpape11@gmail.com>
2025-02-11 19:48:34 -0500

    begin work with CRUD operations

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
 import psycopg2
 import os
@@ -13,13 +13,42 @@ def get_db_connection():
             database=os.getenv('DB'),
             user=os.getenv('DB_USERNAME'),
             password=os.getenv('DB_PASSWORD'))
-    cur = conn.cursor()
-    print('good')
-    return cur
+    return conn
 
 @app.route('/')
 def index():
-    cur = get_db_connection()
-    tst = cur.execute('SELECT * FROM users')
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # Select all products from the table
+    cur.execute("SELECT * FROM users")
+
+    # Fetch the data
+    data = cur.fetchall()
+
+    # close the cursor and connection
     cur.close()
-    return tst
+    conn.close()
+
+    return render_template('index.html', data=data)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO users (username, email, password) VALUES (%s, %s, %s)', (username, email, password))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+if __name__ == '__main__': 
+    app.run(debug=True)
